6d011451bebe9c5373a056f0f7dfebf13c49d25e
Anthony Gruskovnjak <AGruskovnjak@Anthonys-MacBook-Air.local>
2019-09-14 09:51:44 -0400

    attempt at controller

--- New Unity Project/Assets/scripts/Char_controller.cs
+++ New Unity Project/Assets/scripts/Char_controller.cs
@@ -5,25 +5,30 @@ using UnityEngine;
 public class Char_controller : MonoBehaviour
 {
   public float speed = 10.0F;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+  bool jumpDebounce = false; 
 
-    // Update is called once per frame
-    void Update()
-    {
-
-      float translation = Input.GetAxis("Vertical") * speed;
-      float strafe = Input.GetAxis("Horizontal") * speed;
-      translation *= Time.deltaTime;
-      strafe *= Time.deltaTime;
-
-      transform.Translate(strafe,0, translation);
-      if (Input.GetKeyDown("escape")){
-        Cursor.lockState=CursorLockMode.None;
-      }
-        
-    }
+  // Start is called before the first frame update
+  void Start(){
+    Cursor.lockState = CursorLockMode.Locked;
+  }
+  // Update is called once per frame
+  void Update(){
+  	bool jump = Input.GetButtonDown("Jump");
+  	float up = 0.0f;
+  	if(jump && !jumpDebounce){
+  		up = 1.0f * speed;
+  	}
+  	jumpDebounce = jump;
+  	float translation = Input.GetAxis("Vertical") * speed;
+  	float strafe = Input.GetAxis("Horizontal") * speed;
+	
+  	translation *= Time.deltaTime;
+  	strafe *= Time.deltaTime;
+  	transform.Translate(strafe, up, translation);
+	
+  	if (Input.GetKeyDown("escape")){
+  	  Cursor.lockState=CursorLockMode.None;
+  	}
+       
+   }
 }
